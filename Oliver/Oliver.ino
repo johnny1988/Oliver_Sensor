c3f4f593637571d124da1917544f8548a30decdc
Janybasha Shaik <janybasha.shaik@aentron.com>
2020-11-05 17:12:38 +0100

    testing 3 Servos
    added 3 motors and made some x and y and z direction codes

--- Oliver/Oliver.ino
+++ Oliver/Oliver.ino
@@ -18,10 +18,18 @@
 
 /// Start of Definiations
 Servo ServoM;
+Servo ServoMX;
+Servo ServoMY;
+Servo ServoMZ;
+
 MPU6050 mpu;
 // Comment next
 #define BTSerial Serial2 //(17(RX), 16(TX))
 #define PINServo 9
+#define PINServoX 6
+#define PINServoY 7
+#define PINServoZ 8
+
 // Uncomment next
 //SoftwareSerial BTSerial(BT_RX, BT_TX);
 Adafruit_BME280 bme = Adafruit_BME280();
@@ -31,12 +39,18 @@ Adafruit_BME280 bme = Adafruit_BME280();
 #define addr 0x0D /// I2C Address for The HMC5883L
 int     angle = 10;   /// Just dummy Motor Control position angle
 char    serial_buf[100];
+int ax1B = 0;
+int ay1B = 0;
+int az1B = 0;
+
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
 int timer = 0;
 bool        packet_complete_;
 char        packet_[100],
             *packet_ptr_;
+
+
 
 /// End of Parameter Declarations
 
@@ -59,6 +73,10 @@ void setup(void)
   BTSerial.begin(9600);
   delay(500);
   ServoM.attach(PINServo); /// attach the Servo Control on PIN 9
+  ServoMX.attach(PINServoX); /// attach the Servo Control on PIN 9
+  ServoMY.attach(PINServoY); /// attach the Servo Control on PIN 9
+  ServoMZ.attach(PINServoZ); /// attach the Servo Control on PIN 9
+
 
   // Initialize HMC5883L Communication
   Wire.begin();  /// used for HMC5883L Sensor
@@ -105,7 +123,7 @@ void loop(void)
   Serial.println();
   BTSerial.println();
   ////
-  delay(100);
+  delay(1000);
 }
 ///////////////////// MPU Sensor data
 void readMPUData() {
@@ -114,6 +132,55 @@ void readMPUData() {
     sprintf(serial_buf, "X%d|Y%d|Z%d|GX%d|GY%d|GZ%d|", ax, ay, az, gx, gy, gz);
     Serial.println(serial_buf);
     BTSerial.write(serial_buf);
+    int ax1 = map(ax, -17000, 17000, 0, 179);
+    int ay1 =  0;// map(ay, -17000, 17000, 0, 180);
+    int az1 = 0; //map(az, -17000, 17000, 0, 180);
+
+    Serial.print("ax1-");
+    Serial.print(ax1);
+    Serial.print(": ay1-");
+    Serial.print(ay1);
+    Serial.print(": az1-");
+    Serial.print(az1);
+    ServoMX.attach(PINServoX); /// attach the Servo Control on PIN 9
+    ServoMX.write(ax1);
+    delay(400);
+    //ServoMX.detach();
+    if (ax1 - ax1B > 10)
+    {
+      ax1B = ax1;
+
+    }
+    else
+    {
+      ax1 = ax1B;
+    }
+    /////
+    if (ay1 - ay1B > 10)
+    {
+      ay1B = ay1;
+      ServoMY.attach(PINServoY); /// attach the Servo Control on PIN 9
+      ServoMY.write(ay1);
+      delay(400);
+      ServoMY.detach();
+    }
+    else
+    {
+      ay1 = ay1B;
+    }
+    if (az1 - az1B > 5)
+    {
+      az1B = az1;
+      ServoMZ.attach(PINServoZ); /// attach the Servo Control on PIN 9
+      ServoMZ.write(az1);
+      delay(400);
+      ServoMZ.detach();
+    }
+    else
+    {
+      az1 = az1B;
+    }
+    Serial.println("");
   }
 }
 
